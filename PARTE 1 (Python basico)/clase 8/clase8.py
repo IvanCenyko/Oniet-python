4b025e8a141259e03b48ed8da90e30fba819e0cd
IvanCenyko <80519646+IvanCenyko@users.noreply.github.com>
2022-10-25 22:07:48 -0300

    Delete PARTE 1 (Python basico) directory

--- PARTE 1 (Python basico)/clase 8/clase8.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-response = requests.get('https://randomuser.me/api?results=1')
-rjson = response.json()
-edad = rjson['results'][0]['dob']['age']
-pais = rjson['results'][0]['location']['country']
-print(f'su edad es de {edad} años y su pais es {pais}')
