f37364b9cb47bdee72641df919e0cb3b7e1b7375
IvanCenyko <80519646+IvanCenyko@users.noreply.github.com>
2022-10-25 22:22:29 -0300

    Add files via upload

---
+++ PARTE 1 (Python basico)/clase 6 suma de matrices/clase6.py
@@ -0,0 +1,30 @@
+m1 = [
+    [1, 5, 6],
+    [0, -2, 4]
+]
+
+m2 = [
+    [8, -5, 1],
+    [0, 0, 1]
+]
+
+def sumaMatrices(m1, m2):
+
+    aux = []
+    filas = len(m1)
+    for i in range(filas):
+
+        auxFila = []
+        fila = m1[i]
+        columnas = len(fila)
+        for j in range(columnas):
+
+            auxFila.append(fila[j] + m2[i][j])
+
+        aux.append(auxFila)
+
+    return aux
+
+print(sumaMatrices(m1, m2))
+print(m1)
+    
