dbdbacf7156eddcd2504e7fabaed45d9b5eab8d0
IvanCenyko <ivanchicago70@gmail.com>
2023-05-15 13:31:59 -0300

    cleaned

--- simulacros_de_eval/punto_4_palindromos.py
+++
@@ -1,8 +0,0 @@
-word = input("Ingrese palabra: ")
-word = word.replace(",", "")
-word = word.replace(" ", "")
-word = word.lower()
-if str(word) == "".join(reversed(word)) :
-    print("Palindrome")
-else:
-    print("Not Palindrome")
