2f268780f43c0d0eabd5dbbe0f64e070ce4d8300
Iván Cenyko <80519646+IvanCenyko@users.noreply.github.com>
2023-05-14 21:17:52 -0300

    Delete eval_clasificatoria directory

--- eval_clasificatoria/punto_2.py
+++
@@ -1,9 +0,0 @@
-import math
-x1 = float(input("Ingrese x1(m): "))
-y1 = float(input("Ingrese y1(m): "))
-x2 = float(input("Ingrese x2(m): "))
-y2 = float(input("Ingrese y2(m): "))
-
-distancia_total = math.sqrt(abs(x2 - x1)**2 + abs(y2-y1)**2)
-dist_cm = distancia_total * 100
-print("La distancia entre los puntos es de: {:.2f}".format(dist_cm))
