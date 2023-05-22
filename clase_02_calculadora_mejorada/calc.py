793c2e651cd63dbdb437ed63a245c68583d5675d
IvanCenyko <ivanchicago70@gmail.com>
2023-05-22 12:53:23 -0300

    clase 2

---
+++ clase_02_calculadora_mejorada/calc.py
@@ -0,0 +1,30 @@
+# √
+import math
+
+list = [] #lista vacía
+
+list = [7, 6, 4, 5, 7, 'hello']
+var = list[0] #guardo en var lo que haya en el index 0
+list.append(1) #añado el numero 1 a la ultima posición
+list.remove(2) #quito el numero 2 de la lista
+list.pop(0) #quito el index 0 de la lista
+print(list)
+
+# while True:
+#     num1 = float(input("Ingrese un numero: ")) #input numero 1
+#     num2 = float(input("Ingrese un numero: ")) #input numero 2
+#     op = input("Ingresa la operacion: ") #tipo de operacion
+
+#     if op == "+":
+#         print(num1+num2)
+#     elif op == "-":
+#         print(num1-num2)
+#     elif op == "/":
+#         print(num1/num2)
+#     elif op == "*":
+#         print(num1*num2)
+#     elif op == "^":
+#         print(math.pow(num1, num2))
+#     elif op == "√":
+#         print(math.pow(num1, 1/num2))
+
