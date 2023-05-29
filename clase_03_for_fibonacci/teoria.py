c7288354ba7374f6e08ee6df3c06a74caf1929ad
IvanCenyko <ivanchicago70@gmail.com>
2023-05-29 14:13:48 -0300

    Clase 3

---
+++ clase_03_for_fibonacci/teoria.py
@@ -0,0 +1,15 @@
+n = 10 #cantidad de repeticiones
+
+# for que se repite "n" veces
+# la variable "i" toma el numero de repeticion en ese momento
+for i in range(n):
+    print(i)
+
+print('----------------') #separador
+
+lista = [0, 2, 4, 5, 6]
+
+# for que se repite una vez por cada elemento de la lista
+# "i" toma cada valor de la lista uno por uno
+for i in lista:
+    print(i)
