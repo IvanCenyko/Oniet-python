0ee1967e1bd73a42f3f2f37d44b151c1db546135
IvanCenyko <ivanchicago70@gmail.com>
2023-06-05 10:01:32 -0300

    clase_04

---
+++ clase_03_funcion_billetes/clase_03.py
@@ -0,0 +1,16 @@
+def vuelto(cantidad, billetes):
+    
+    vuelto = []
+    billetes.sort(reverse=True)
+
+    for b in billetes:
+        cant_billetes = cantidad // b
+        cantidad -= cant_billetes * b
+        vuelto.append(cant_billetes)
+        
+        if cantidad == 0:
+            break
+    
+    return vuelto
+
+print(vuelto(347, [100, 50, 20, 1]))
